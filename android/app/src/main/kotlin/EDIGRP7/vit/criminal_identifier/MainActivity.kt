d292016cfcd512844fde3bfbbb0cd25942248799
RehanQazi76 <rehanqazi02@gmail.com>
2021-11-18 19:31:41 +0530

    fire base done

---
+++ android/app/src/main/kotlin/EDIGRP7/vit/criminal_identifier/MainActivity.kt
@@ -0,0 +1,6 @@
+package EDIGRP7.vit.criminal_identifier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+Future main () async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: Home(),));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('welcome '),
+      ),
+      body: Center(
+        child:Container(
+          child:Column(
+            children: <Widget>[
+              Container(
+                child:const Image(image: AssetImage("images/pic1.jpg"),),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: CupertinoColors.black,
+                  onPrimary: CupertinoColors.white,
+                ),
+                child: const Text('Login'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginP()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
